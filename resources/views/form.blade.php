cc83304c892da04abdea3f42a74a85fc2e54e2cb
Betül <84292454+sebetulse@users.noreply.github.com>
2021-06-08 23:05:46 +0300

    Update form.blade.php

--- resources/views/form.blade.php
+++ resources/views/form.blade.php
@@ -4,12 +4,11 @@
 <div class="row">
     
 </div>
-
-<script data-b24-form="inline/8/vbxope" data-skip-moving="true">
+<script data-b24-form="inline/1/hgtxri" data-skip-moving="true">
         (function(w,d,u){
                 var s=d.createElement('script');s.async=true;s.src=u+'?'+(Date.now()/180000|0);
                 var h=d.getElementsByTagName('script')[0];h.parentNode.insertBefore(s,h);
-        })(window,document,'https://cdn.bitrix24.com/b17765935/crm/form/loader_8.js');
+        })(window,document,'https://cdn.bitrix24.com/b17787057/crm/form/loader_1.js');
 </script>
 
 
