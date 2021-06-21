ed31c3a34cbd0f47c8acaf7808824204ad3a73b8
Betül <84292454+sebetulse@users.noreply.github.com>
2021-06-21 16:20:15 +0300

    Update form.blade.php

--- resources/views/form.blade.php
+++ resources/views/form.blade.php
@@ -18,14 +18,12 @@
         })(window,document,'https://cdn.bitrix24.com/b17765935/crm/site_button/loader_4_6gye0p.js');
 </script>
 
-<script>
-document.querySelector('form').setAttribute('autocomplete', 'off');
-let inputList= document.querySelectorAll('input');
-for(var i=0; i<inputList.length; i++){
-    inputList[i].removeAttribute('autocomplete')
-}
+<script data-b24-form="inline/14/zb9vz9" data-skip-moving="true">
+        (function(w,d,u){
+                var s=d.createElement('script');s.async=true;s.src=u+'?'+(Date.now()/180000|0);
+                var h=d.getElementsByTagName('script')[0];h.parentNode.insertBefore(s,h);
+        })(window,document,'https://cdn.bitrix24.com/b17765935/crm/form/loader_14.js');
 </script>
-
 
 
 
